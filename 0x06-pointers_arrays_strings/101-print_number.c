9b7e4c6773aa769ee097023e2e9380d6af3ca4b2
clivemusya <clivemusya@gmail.com>
2022-10-28 13:32:08 -0700

    point

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_number - Prints
+ *@n:var
+ *Return: Null
+ */
+
+void print_number(int n)
+{
+unsigned int temp;
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+temp = n;
+if (temp / 10)
+print_number(temp / 10);
+_putchar(temp % 10 + '0');
+}
