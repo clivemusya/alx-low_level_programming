92393ad9299733a92f6a76d0377d75515896cf2f
clivemusya <clivemusya@gmail.com>
2022-11-10 10:59:31 -0800

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - changes
+ *@grid:var
+ *@height:var
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+}
